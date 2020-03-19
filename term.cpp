d25fb8ef7256daff56567d72e0743337227b2879
Deophius Invinsa <NO1zhuce@163.com>
2020-03-19 13:03:57 +0800

    Made the terminator more friendly to users.
    1. Added an information line that says what this program is doing.
    2. Redirected output of underlying commands to nul.
    3. Pausing for 0.5s after the job is done.

--- term.cpp
+++ term.cpp
@@ -1,8 +1,12 @@
 #include <cstdlib>
+#include <windows.h>
+#include <cstdio>
 
 int main() {
     // Terminate the rollcall clicker in case of emergency.
     // Recommended on the task bar.
-    std::system("taskkill /f /im rollcall.exe");
-    std::system("taskkill /f /im jumpstart.exe");
+    std::printf("Terminating rollcall.exe and jumpstart.exe.\n");
+    std::system("taskkill /f /im rollcall.exe > NUL 2> NUL");
+    std::system("taskkill /f /im jumpstart.exe > NUL 2> NUL");
+    Sleep(500);
 }
