b47a1ea865c7cff4370186eb936d1fa89b28cc2e
Deophius Invinsa <NO1zhuce@163.com>
2020-03-19 12:57:43 +0800

    Added killing jumpstart.exe to terminator
    After the tragedy of chemistry class.

--- term.cpp
+++ term.cpp
@@ -4,4 +4,5 @@ int main() {
     // Terminate the rollcall clicker in case of emergency.
     // Recommended on the task bar.
     std::system("taskkill /f /im rollcall.exe");
+    std::system("taskkill /f /im jumpstart.exe");
 }
